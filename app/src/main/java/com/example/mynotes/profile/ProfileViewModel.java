44eea58f3c064b8128959978722d6b6fa39caca5
V8299353 <v8299353@live.tees.ac.uk>
2022-12-13 19:35:40 +0000

    dependencies updated

--- app/src/main/java/com/example/mynotes/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/mynotes/profile/ProfileViewModel.java
@@ -12,7 +12,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-//import com.google.firebase.ktx.Firebase;
 
 public class ProfileViewModel  extends ViewModel {
 
